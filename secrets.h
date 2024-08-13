5fdf4753073daffc88f8a18ba04fe67e4d60e8fc
jonpul <jonpul@msn.com>
2024-08-13 15:30:04 -0500

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,6 @@
+// Replace with your network details
+const char* SSID = "<SSID>";
+const char* SSID_PASSWORD = "<PWD>";
+
+const char* THOUGHT_USERID = "<user ID>";
+const char* THOUGHT_TOKEN = "<token>";   // https://www.stands4.com/services/v2/quotes.php?uid=<user ID>&tokenid=<token>>&format=json
